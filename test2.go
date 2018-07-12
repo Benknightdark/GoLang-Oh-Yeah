7645cc4bcd1236c42dd3dd7d6ebc2f183b729d7e
BenKung <moon791017@gmail.com>
2018-07-12 10:54:58 +0800

    OK

--- test2.go
+++ test2.go
@@ -18,6 +18,19 @@ const (
 	z = iota
 )
 
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func SumAndProduct(a, b int) (int, int) {
+	return a + b, a * b
+}
+func p(s interface{}) {
+	fmt.Println(s)
+}
+
 func main() {
 	fmt.Println(i)
 	fmt.Println(x)
@@ -31,8 +44,41 @@ func main() {
 	m := make(map[string]string)
 	m["hello"] = "bbb"
 	p(m["hello"])
+	delete(m, "hello")
+	p(m)
+	m1 := m
+	m1["hello"] = "ccc"
+	p(m)
+	p(m1)
 
-}
-func p(s interface{}) {
-	fmt.Println(s)
+	sum := 0
+	for index := 0; index < 10; index++ {
+		sum += index
+	}
+	p(sum)
+
+	sum2 := 1
+	for sum2 < 1000 {
+		sum2 += sum2
+	}
+	p(sum2)
+
+	for k, v := range m {
+		p(k)
+		p(v)
+	}
+
+	for _, v := range m {
+
+		p(v)
+	}
+	x := 1
+	y := 2
+	z := 3
+	p(max(x, y))
+	p(max(z, y))
+	sumxy, meanxy := SumAndProduct(x, y)
+	p(sumxy)
+	p(meanxy)
+
 }
